9d9f1affa9c881ac878482f9321af41f73bf1f36
Amit Kumar Pradhan <akpradhn@gmail.com>
2017-03-10 01:47:45 +0530

    Create __init__.py
    init py commit

---
+++ CNNex01/__init__.py
@@ -0,0 +1,10 @@
+# Reference
+# https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/__init__.py
+
+"""Makes helper libraries available in the cifar10 package."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cifar10
+import cifar10_input

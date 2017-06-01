c2a435e6daf8ca0344e648f2166a59e4589d7734
Amit Kumar Pradhan <akpradhn@gmail.com>
2017-06-01 16:31:05 +0530

    code to start a gui in python
    - a simple gui

---
+++ PyGUI/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 16:24:41 2017
+
+@author: amit
+"""
+
+#import library tkinter
+
+from tkinter import *
+
+# create a window
+root = Tk()
+
+# Degine Window 
+root.title("Welcome To PyGUI")
+root.geometry("400x300")
+
+# Start event loop
+root.mainloop()

773371711797a4ffe301e92dc36d761caa173c59
akpradhn <akpradhn@gmail.com>
2017-12-16 09:16:11 +0530

    intro

--- TensorFlow/intro.py
+++ TensorFlow/intro.py
@@ -3,3 +3,25 @@
 # Array Manupulation of any size
 # Tensorflow
 # Function on a tensor
+
+import tensorflow as tf 
+
+# Part 1: Computation Graph
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+# Note : tf.mul is replaced ith tf.multiply
+# result = x1 * x2
+result = tf.multiply(x1,x2)
+print(result)
+
+# Part 2: Tensor Session
+#sess = tf.Session()
+#print(sess.run(result))
+#sess.close()
+
+# To close the session automatically
+with tf.Session() as sess:
+	output = sess.run(result)
+	print(output)
+	
